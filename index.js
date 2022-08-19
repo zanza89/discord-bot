4ee01717aee33d617d87d9f1c347ff338c12636a
ducanhduong <wantanboy89@gmail.com>
2022-08-19 10:09:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+		// await interaction.reply('Pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply('Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}');
+	}
+	else if (commandName === 'user') {
+		await interaction.reply('User tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}');
+	}
+});
+
+// Login to Discord with your client's token
+client.login(token);
