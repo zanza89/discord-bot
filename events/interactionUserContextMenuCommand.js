b6c40ead80c0b3279d873d7120742bb7bc6bc769
ducanhduong <wantanboy89@gmail.com>
2022-08-27 19:29:21 +0200

    bug fixes

--- events/interactionUserContextMenuCommand.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'interactionUserContextMenuCommand',
-	execute(interaction) {
-		const name = interaction.targetUser.username;
-		console.log('user ' + name + ' in ' + interaction.channel.name + ' responded to survey with ' + interaction.targetMessage);
-	},
-};
