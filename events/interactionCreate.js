c013467d190f9c5f2b44b9f5e76f28a89c34b9db
ducanhduong <wantanboy89@gmail.com>
2022-08-27 17:13:27 +0200

    fixed bug

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
-		if (interaction.isChatInputCommand()) return;
+		if (!interaction.isChatInputCommand()) return;
 		console.log(interaction.user.tag + ' in ' + interaction.channel.name + 'triggered an interaction.');
 	},
 };
