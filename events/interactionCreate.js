ce9533401405f6f0def749a49c5a67d08f96ff57
ducanhduong <wantanboy89@gmail.com>
2022-08-27 18:45:16 +0200

    modified event 'interactionCreate'

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,13 +1,6 @@
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
 		console.log(interaction.user.tag + ' in ' + interaction.channel.name + ' triggered an interaction.');
-
-		if (!interaction.isButton()) return;
-		console.log(interaction);
-
-		if (!interaction.isSelectMenu()) return;
-		console.log(interaction);
 	},
 };
