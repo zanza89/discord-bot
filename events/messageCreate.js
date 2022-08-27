237c9b15ca709b0eb1cf83f767f377b932c9f17a
ducanhduong <wantanboy89@gmail.com>
2022-08-28 00:31:16 +0200

    bug fixes

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,5 +2,7 @@ module.exports = {
 	name: 'messageCreate',
 	execute(message) {
 		console.log(message.content);
+		console.log(message.createdAt.toDateString());
+		console.log(message.author.tag);
 	},
 };
