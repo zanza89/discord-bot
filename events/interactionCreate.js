1f0bea1399c2c5be580cf66acb1550ed5a3bcbda
ducanhduong <wantanboy89@gmail.com>
2022-08-28 23:31:00 +0200

    bug fixes

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,7 +8,7 @@ module.exports = {
 		if (interaction.customId === 'select_logo') {
 			interaction.update({ content: 'Du hast abgestimmt!', components: [] });
 			console.log(interaction.values);
-			interaction.followUp(interaction.user.tag + 'voted for Design' + interaction.value);
+			interaction.channel.send(interaction.user.tag + 'voted for Design' + interaction.value);
 		}
 	},
 };
