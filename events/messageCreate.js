97de04b29134a9681054c4db5f485648e96becd8
ducanhduong <wantanboy89@gmail.com>
2022-08-28 21:22:25 +0200

    bug fixes

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -10,13 +10,13 @@ module.exports = {
 
 		// use collection class
 		if (message.author.bot) return;
-		if (message.content.toLowerCase() === '?listen') {
-			message.channel.send('bot is collecting messages now..');
-			const filter = m => m.content.includes('test');
-			const collector = new discord.MessageCollector(message.channel, filter);
-			collector.on('collect', m => {
-				console.log('collected message: ' + m.content);
-			});
-		}
+
+		// message.channel.send('bot is collecting messages now..');
+		const filter = m => m.content.includes('test');
+		const collector = new discord.MessageCollector(message.channel, filter);
+		collector.on('collect', m => {
+			console.log('collected message: ' + m.content);
+		});
+
 	},
 };
