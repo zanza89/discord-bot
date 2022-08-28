615b37cd04d72cedbc1e1e965d6d530b5a329845
ducanhduong <wantanboy89@gmail.com>
2022-08-28 22:28:16 +0200

    bug fixes

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,5 @@
-const discord = require('discord.js');
-const { ComponentType } = require('discord.js');
+// const discord = require('discord.js');
+const { ComponentType, InteractionCollector } = require('discord.js');
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction, client) {
@@ -10,7 +10,7 @@ module.exports = {
 		if (interaction.customId === 'select_logo') {
 			interaction.update({ content: 'something was selected', components: [] });
 			console.log(interaction);
-			const collector = discord.InteractionCollector({ client, componentType: ComponentType.SelectMenu });
+			const collector = new InteractionCollector({ client, componentType: ComponentType.SelectMenu });
 			collector.on('collect', i => {
 				if (i.user.id === interaction.user.id) {
 					i.reply(i.user.id + 'clicked on the ' + i.customId + ' button.');
