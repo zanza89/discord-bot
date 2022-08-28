47745058621b4f9369e703631fc1d09c1b6b4c63
ducanhduong <wantanboy89@gmail.com>
2022-08-28 23:15:23 +0200

    bug fixes

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'interactionCreate',
-	execute(interaction) {
+	execute(interaction, client) {
 		console.log(interaction.user.tag + ' in ' + interaction.channel.name + ' triggered an interaction.');
 
 		// selectMenu handling
@@ -8,7 +8,7 @@ module.exports = {
 		if (interaction.customId === 'select_logo') {
 			interaction.update({ content: 'something was selected', components: [] });
 			console.log(interaction.values);
-			const destination = interaction.channel.get('1013514283998523483');
+			const destination = client.channels.cache.get('1013514283998523483');
 			destination.send(interaction.user.tag + 'voted for Design' + interaction.value);
 		}
 	},
