8cf79536d2f2cf4dd9cd3f5dcad893504aa0d80c
ducanhduong <wantanboy89@gmail.com>
2022-08-29 21:47:24 +0200

    bug fixes

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'guildMemberAdd',
 	execute(member) {
-		console.log('New User ' + member.user.username + ' has joined ' + member.guild.name);
+		console.log('New User ' + member.user.tag + ' has joined ' + member.guild.name);
 		member.guild.channels.cache.find(c => c.name === 'verifizierung').send(member.user.tag + 'has joined this server');
 	},
 };
