a5aabb559674f44c3610d99bd99e54a42afe54c9
ducanhduong <wantanboy89@gmail.com>
2022-09-02 23:05:09 +0200

    test

--- commands/testapi.js
+++ commands/testapi.js
@@ -4,7 +4,7 @@ const { fetch } = require('undici');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('testapi')
-		.setDescription('test to fetch da api json'),
+		.setDescription('test to fetch da api json using undici'),
 	async execute(interaction) {
 		const data = { active: 'example' };
 		const channel = interaction.client.channels.cache.get('1009958552892346378');
@@ -20,21 +20,14 @@ module.exports = {
 			// eslint-disable-next-line no-shadow
 			.then((data) => {
 				console.log('Success:', data);
-				// const reviver = function(value) {
-				// 	if (typeof value === 'undefined') { return null; }
-				// 	if (value.match('<') || value.match('>') || value.match('z')) {
-				// 		return 'aaaaa';
-				// 	}
-				// };
 				try {
-					// const zanza = '921a8e63-79b6-416d-a424-e62d74211633';
 					const property_keys = Object.keys(data);
 					const property_values = Object.values(data);
 					const property_entries = Object.entries(data);
 
-					// const property_players = Object.keys(data.data);
-					// const property_valuetest1 = Object.values(data.data);
-					// const property_entries_data = Object.entries(data.data);
+					const property_players = Object.keys(data.data);
+					const property_valuetest1 = Object.values(data.data);
+					const property_entries_data = Object.entries(data.data);
 
 					// const property_players_serverparagon = Object.keys(data.data.server_paragon);
 					// const property_value_serverparagon = Object.values(data.data.server_paragon);
@@ -43,9 +36,9 @@ module.exports = {
 					console.log('property_values: ' + property_values);
 					console.log('property_entries (Array): ' + property_entries);
 
-					// console.log('property_players: ' + property_players);
-					// console.log('property_players values: ' + property_valuetest1);
-					// console.log('property_players entries (Array): ' + property_entries_data);
+					console.log('property_players: ' + property_players);
+					console.log('property_players values: ' + property_valuetest1);
+					console.log('property_players entries (Array): ' + property_entries_data);
 
 					// console.log('property_players serverparagon: ' + property_players_serverparagon);
 					// console.log('property_players values serverparagon: ' + property_value_serverparagon);
