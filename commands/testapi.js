22be88ab95f103db8e8aca441d00cebf9ca5dc2f
ducanhduong <wantanboy89@gmail.com>
2022-09-02 18:06:12 +0200

    bug fixes

--- commands/testapi.js
+++ commands/testapi.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('testapi')
 		.setDescription('test to fetch da api json'),
 	async execute(interaction) {
-		const data = { username: 'example' };
+		const data = { active: 'example' };
 		const channel = interaction.client.channels.cache.get('1009958552892346378');
 		fetch('https://immortal.zwoggel.org/api/public/playerlist', {
 			// eslint-disable-next-line no-inline-comments
@@ -26,11 +26,42 @@ module.exports = {
 				// 	}
 				// };
 				try {
-					const data_parsed = JSON.parse(data);
-					console.log('data parsed: ' + data_parsed);
+					// const zanza = '921a8e63-79b6-416d-a424-e62d74211633';
+					const property_keys = Object.keys(data);
+					const property_values = Object.values(data);
+					const property_entries = Object.entries(data);
+
+					const property_players = Object.keys(data.data);
+					const property_valuetest1 = Object.values(data.data);
+					const property_entries_data = Object.entries(data.data);
+
+					const property_players_serverparagon = Object.keys(data.data.server_paragon);
+					const property_value_serverparagon = Object.values(data.data.server_paragon);
+
+					console.log('property_keys: ' + property_keys);
+					console.log('property_values: ' + property_values);
+					console.log('property_entries (Array): ' + property_entries);
+
+					console.log('property_players: ' + property_players);
+					console.log('property_players values: ' + property_valuetest1);
+					console.log('property_players entries (Array): ' + property_entries_data);
+
+					console.log('property_players serverparagon: ' + property_players_serverparagon);
+					console.log('property_players values serverparagon: ' + property_value_serverparagon);
+					let collection = [];
+					for (let i = 0; i < property_entries_data.length; i++) {
+						for (let j = 0; j < property_entries_data[i].length; j++) {
+							console.log('element_entry: ' + i + ' : ' + j + ' = ' + property_entries_data[i][j]);
+							collection = [i][1];
+							console.log('collection: ' + JSON.stringify(collection));
+						}
+					}
+
+					// const stringified = JSON.stringify(property_entries);
+					// console.log('stringified: ' + stringified);
 				}
 				catch (error) {
-					console.log('parse not successful');
+					console.log('reading object not successful');
 					console.log(error);
 				}
 			})
