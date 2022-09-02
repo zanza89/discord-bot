1cd84f7875d87623a341fa1d2bc5dbb6c812563d
ducanhduong <wantanboy89@gmail.com>
2022-09-03 00:22:10 +0200

    test7

--- commands/testapi2.js
+++ commands/testapi2.js
@@ -18,7 +18,7 @@ module.exports = {
 		const immortalResult = await request('https://immortal.zwoggel.org/api/json/charlist');
 		// const char_name = 'char_name';
 		// const data = 'data';
-		const ok = 'ok';
+		const ok = 'message';
 		const response = await getJSONResponse(immortalResult.message[ok]);
 		try {
 			const property_keys = Object.keys(response);
