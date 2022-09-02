c434701a15842180f9c84e81baedcc0b17eff199
ducanhduong <wantanboy89@gmail.com>
2022-09-03 01:35:40 +0200

    cat fix

--- commands/testapi2.js
+++ commands/testapi2.js
@@ -15,21 +15,25 @@ module.exports = {
 
 			return JSON.parse(fullBody);
 		}
-		const immortalResult = await request('https://immortal.zwoggel.org/api/json/charlist');
-		const data = await getJSONResponse(immortalResult.data);
-		try {
-			const property_keys = Object.keys(data);
-			const property_values = Object.values(data);
-			const property_entries = Object.entries(data);
+		// const immortalResult = await request('https://immortal.zwoggel.org/api/json/charlist');
+		// const data = await getJSONResponse(immortalResult.data);
+		// try {
+		// 	const property_keys = Object.keys(data);
+		// 	const property_values = Object.values(data);
+		// 	const property_entries = Object.entries(data);
 
-			console.log('keys: ' + property_keys);
-			console.log('values: ' + property_values);
-			console.log('entries: ' + property_entries);
-		}
-		catch (error) {
-			console.log('there was a mistake happened');
-			console.log(error);
-		}
-		interaction.deferReply('...');
+		// 	console.log('keys: ' + property_keys);
+		// 	console.log('values: ' + property_values);
+		// 	console.log('entries: ' + property_entries);
+		// }
+		// catch (error) {
+		// 	console.log('there was a mistake happened');
+		// 	console.log(error);
+		// }
+		// interaction.deferReply('...');
+
+		const catResult = await request('https://aws.random.cat/meow');
+		const { file } = await getJSONResponse(catResult.body);
+		interaction.editReply({ files: [file] });
 	},
 };
