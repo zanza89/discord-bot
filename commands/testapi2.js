8fda7cda1036b456585e3a63a60b82fae1094c6d
zanza89 <wantanboy89@gmail.com>
2022-09-03 23:20:09 +0200

    test

--- commands/testapi2.js
+++ commands/testapi2.js
@@ -85,7 +85,7 @@ module.exports = {
 
 		for (let i = 0; i < counter; i++) {
 			arrmerge[i] = charnames[i];
-			for (let j = 0; j < 1; j++) {
+			for (let j = 0; j == 1; j++) {
 				arrmerge[i][j] = discordids[i];
 			}
 		}
