bc87b09b2537353720b79493935c67d546c2f634
zanza89 <wantanboy89@gmail.com>
2022-09-03 14:12:29 +0200

    test

--- commands/testapi2.js
+++ commands/testapi2.js
@@ -52,7 +52,7 @@ module.exports = {
 		+ '\nparagon level : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].paragon_level
 		+ '\ndiscordId : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].discord_id
 		+ '\nlast updated : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].last_update);
-		const arr = Object.entries(data['984cefe0-5531-4690-919c-fa51d6009f48'].char_name);
+		const arr = Object.entries(data['984cefe0-5531-4690-919c-fa51d6009f48']);
 		console.log('arr = ' + arr);
 	},
 };
