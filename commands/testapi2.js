16814722d6070b57eca2f0956c1628094bb75129
zanza89 <wantanboy89@gmail.com>
2022-09-04 12:44:18 +0200

    created dir data

--- commands/testapi2.js
+++ commands/testapi2.js
@@ -14,53 +14,14 @@ module.exports = {
 			}
 			return JSON.parse(fullBody);
 		}
-		// const immortalResult = await request('https://immortal.zwoggel.org/api/json/charlist');
-		// const data = await getJSONResponse(immortalResult.data);
-		// try {
-		// 	const property_keys = Object.keys(data);
-		// 	const property_values = Object.values(data);
-		// 	const property_entries = Object.entries(data);
 
-		// 	console.log('keys: ' + property_keys);
-		// 	console.log('values: ' + property_values);
-		// 	console.log('entries: ' + property_entries);
-		// }
-		// catch (error) {
-		// 	console.log('there was a mistake happened');
-		// 	console.log(error);
-		// }
-		// interaction.deferReply('...');
-
-		const result = await request('https://immortal.zwoggel.org/api/json/charlist');
+		const filePath = '/data';
+		const url = 'https://immortal.zwoggel.org/api/json/charlist';
+		const result = await request(url);
 		const { message, success, current_time, data } = await getJSONResponse(result.body);
 
-		console.log('message : ' + message);
-		console.log('success : ' + success);
-		console.log('current_time : ' + current_time);
-		console.log('data: ' + data);
-		console.log('server_paragon : ' + data.server_paragon);
-		console.log('char_name: ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].char_name);
-		console.log('active : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].active);
-		console.log('twink : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].twink);
-		console.log('paragon_level : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].paragon_level);
-		console.log('discord_id : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].discord_id);
-		console.log('last_update : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].last_update);
-		interaction.reply(''
-		+ 'char_name : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].char_name
-		+ '\nactive : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].active
-		+ '\ntwink : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].twink
-		+ '\nparagon level : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].paragon_level
-		+ '\ndiscordId : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].discord_id
-		+ '\nlast updated : ' + data['984cefe0-5531-4690-919c-fa51d6009f48'].last_update);
-		// const [char_name, active, twink, paragon_level, discord_id, last_update] = Object.entries(data['984cefe0-5531-4690-919c-fa51d6009f48']);
-		// console.log('char_name_tag' + ' = ' + char_name);
-		// console.log('active_tag' + ' = ' + active);
-		// console.log('twink_tag' + ' = ' + twink);
-		// console.log('paragon_level_tag' + ' = ' + paragon_level);
-		// console.log('discord_id_tag' + ' = ' + discord_id);
-		// console.log('last_update_tag' + ' = ' + last_update);
-		// console.log(discord_id[0, 1]);
-		// keys array
+		console.log('saved array tp path ' + filePath);
+		interaction.reply('api fetched from url' + url + '\nmessage : ' + message + '\nsuccess : ' + success + '\nserver_time' + current_time);
 		let keys = Array.apply(null, Array(100));
 		keys = Object.keys(data).splice(1);
 		console.log('keys: ' + keys + '\nsize: ' + keys.length);
@@ -105,7 +66,7 @@ module.exports = {
 		}
 		// let newarray = [];
 		for (let i = 0; i < charnames.length; i++) {
-			console.table(createAndFillTwoDArray({ rows:charnames.length, columns:2, value: charnames[i] }));
+			createAndFillTwoDArray({ rows:charnames.length, columns:2, value: charnames[i] });
 
 		}
 
