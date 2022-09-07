89a70982a7bc2f07c2081ecf1c7ca8b5f4923ecc
ducanhduong <wantanboy89@gmail.com>
2022-09-07 23:46:38 +0200

    test

--- commands/testapipwrecovery.js
+++ commands/testapipwrecovery.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { fetch } = require('undici');
-const { Readable } = require('node:stream');
+import { Readable } from 'node:stream';
 
 module.exports = {
 	data: new SlashCommandBuilder()
