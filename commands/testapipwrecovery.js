136cdb592bae66bfbd33bcd8a3ea8cb1bc099831
ducanhduong <wantanboy89@gmail.com>
2022-09-07 16:23:13 +0200

    added command testapipwrecovery

---
+++ commands/testapipwrecovery.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { fetch } = require('undici');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('testapipwrecovery')
+		.setDescription('password recovery')
+		.addUserOption(option =>
+			option
+				.setName('target')
+				.setDescription('the user targeted')
+				.setRequired(true)),
+	async execute(interaction) {
+		async function getJSONResponse(targetId) {
+			const api = 'https://immortal.zwoggel.org/api/json/reset_pw';
+			const rsp = await fetch(api, {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json',
+				},
+				body: JSON.stringify({
+					discord_id: targetId,
+					auth_token: 'ThisIsAToken',
+				}),
+			});
+			if (rsp.status !== 200) {
+				console.log(rsp.status, 'Anfrage fehlgeschlagen');
+				return;
+			}
+			const json = await rsp.json();
+			console.log(rsp.status, json);
+		}
+		const targetId = interaction.options.getUser('target').id;
+		getJSONResponse(targetId);
+	},
+};
